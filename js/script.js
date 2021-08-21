2e0509720c375594f328698929add847b722c8e5
soumya <saurabh@nettrackers.net>
2021-08-21 13:03:46 +0530

    commit upload file

--- js/script.js
+++ js/script.js
@@ -45,7 +45,27 @@ $(document).ready(function(){
     
     
      
-    
+    $('#client_speak').owlCarousel({
+         	loop:true,
+         	margin:10,
+         	nav:true,
+         	autoplay:true,
+         	navText:["<i class='fa fa-angle-left'></i>","<i class='fa fa-angle-right'></i>"],
+         	responsive:{
+         		0:{
+         			items:1
+         		},
+         		480:{
+         			items:1
+         		},
+         		600:{
+         			items:2
+         		},
+         		1000:{
+         			items:2
+         		}
+         	} 
+         });
     
     
   
